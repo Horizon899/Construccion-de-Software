bc44123c637d851f313f9b108397c9af36a98a56
Horizon899 <gaelcasares899@gmail.com>
2024-08-08 13:24:42 -0600

    Funciones modernas

--- Lab 04.js
+++ Lab 04.js
@@ -8,7 +8,7 @@ console.assert(1==1);
 let nombre = "Gael";
 console.log("Hola " + nombre);
 
-const precio = 100;
+const raza = "Chow Chow";
 // Alcance de las variables
 {
     let Perros=20;
@@ -16,14 +16,43 @@ const precio = 100;
 }
 
 // alert, prompt, confirm
-alert('Los perros cuestan ${precio}'); 
+alert('Los perros son ${raza}'); 
 const chilak_fav = prompt("Cuales son tus perros favoritos?");
 console.log('Tus perros favoritos son ${chilak_fav}');
 
-const is_hambre = confirm("Quieres un perro?")
+const is_hambre = confirm("Quieres un perro?");
 
 if(is_hambre){
     console.info("Adopta un perro");
 } else {
     console.warn("Regresa cuando quieras uno");
 }
+
+// funciones
+function adoptar_perro(raza){
+    console.log('Buscando perro de raza ${raza}');
+}
+
+adoptar_perro(raza);
+
+// funciones modernas
+
+() => {
+    console.log("Perros merequetengos");
+}
+
+document.getElementById("Picale").onclick = () => {
+    console.log("No pensé que lo ibas a presionar, no hay nada aquí pero felicidades, le picaste.");
+}
+
+const arreglo = ["Elemento"];
+
+const arreglo2 = new Array();
+
+arreglo.push("Otro elemento");
+
+arreglo[10] = "Uno más";
+
+arreglo["Perros"];
+
+const objeto = {};
